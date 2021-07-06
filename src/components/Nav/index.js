fa221804aa8da84e6e9b1aa86ef67c96a0d12c91
Shay Wagner <srwagner916@gmail.com>
2021-07-06 13:44:23 -0500

    lifted state to App.js

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,29 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { capitalizeFirstLetter } from '../../utils/helpers';
 
-function Nav() {
-  const categories = [
-    {
-      name: "commercial",
-      description:
-        "photos of grocery stores, food trucks, and other commercial projects"
-    },
-    { name: "portraits", description: "Portraits of people in my life" },
-    { name: "food", description: "Delicious delicaciers" },
-    {
-      name: "landscape",
-      description: "Fields, farmhouses, waterfalls, and the beauty of nature",
-    },
-  ];
+function Nav(props) {
+  const {
+    categories = [],
+    setCurrentCategory,
+    currentCategory,
+  } = props;
+  
+  useEffect(() => {
+    document.title = capitalizeFirstLetter(currentCategory.name);
+  }, [currentCategory]);
 
-  function categorySelected(name) {
-    console.log(`${name} clicked`);
-  }
-
+    
   return (
-    <header>
+    <header className="flex-row px-1">
       <h2>
         <a data-testid="link" href="/">
-          <span role="img" aria-label="camera"> 📸</span> Oh Snap!
+          <span role="img" aria-label="camera">📸</span> Oh Snap!
         </a>
       </h2>
       <nav>
@@ -38,18 +32,24 @@ function Nav() {
           </li>
           {categories.map((category) => (
             <li
-              className="mx-1"
+              className={`mx-1 ${
+                currentCategory.name === category.name && 'navActive'
+                }`}
               key={category.name}
             >
-              <span onClick={() => categorySelected(category.name)}>
-                {category.name}
+              <span
+                onClick={() => {
+                  setCurrentCategory(category)
+                }}
+              >
+                {capitalizeFirstLetter(category.name)}
               </span>
             </li>
           ))}
         </ul>
       </nav>
     </header>
-  )
+  );
 }
 
 export default Nav;
