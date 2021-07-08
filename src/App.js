c291883cef86f2b099aa7dae65c44ef543f5a0e8
Shay Wagner <srwagner916@gmail.com>
2021-07-08 15:52:55 -0500

    conditionally render components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Nav from './components/Nav';
 import Gallery from './components/Gallery';
 import ContactForm from './components/Contact';
 
+
 function App() {
   const [categories] = useState([
     {
@@ -14,20 +15,29 @@ function App() {
     { name: 'food', description: 'Delicious delicacies' },
     { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
   ]);
-
+  
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
-
+  
+  const [contactSelected, setContactSelected] = useState(false);
+  
   return (
     <div>
       <Nav
         categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected= {setContactSelected}
       ></Nav>
       <main>
-        <ContactForm></ContactForm>
-        <Gallery currentCategory={currentCategory}></Gallery>
-        <About></About>
+        {!contactSelected ? (
+          <>
+            <Gallery currentCategory={currentCategory}></Gallery>
+            <About></About>
+          </>
+        ) : (
+            <ContactForm></ContactForm>
+        )}
       </main>
     </div>
   );
